ef619ea27839297638cbb8958dfb92eb750a1161
KaoWebDesign <assdwqe@gmail.com>
2019-05-04 01:37:29 +0300

    initial

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-app>
+            <v-toolbar app dark class="primary">
+                <v-toolbar-title  v-text="'your-english'"></v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-toolbar-items>
+                    <v-btn>Button1</v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
+        </v-app>
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+<style scoped>
+</style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+    state: {
 
-  },
-  mutations: {
+    },
+    mutations: {
 
-  },
-  actions: {
+    },
+    actions: {
 
-  }
+    }
 })

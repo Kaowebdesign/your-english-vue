ca5d40a75292a322c10410412f977b1934544c6a
kaowebdesign <assdwqe@gmail.com>
2019-06-07 17:29:44 +0300

    upd article item | new design to cards

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -1,35 +1,25 @@
 <template>
-            <!-- <v-flex xs4 md3>
-                <v-card-media :src="article.imageUrl">
-                </v-card-media>
-            </v-flex>
-            <v-flex xs8 md9>
-                <v-card-title>
-                    <div>
-                        <div><h2>{{article.title}}</h2></div>
-                        <div><p>{{article.description}}</p></div>
-                        <div><p>Рівень: {{getArticleLevel(article.level)}} , {{article.parts.length}} частей</p> </div>
+    <v-card class="ma-4 bookCard">
+        <v-card-media :src="article.imageUrl" height="450px"></v-card-media>
+        <div class="bookCard__titleWrap">
+            <v-card-title>
+                <div>
+                    <div class="bookCard__captionWrap">
+                        <h3 class="display-1 mb-2 bookCard__text">{{article.title}}</h3>
                     </div>
-                </v-card-title>
+                    <div><p class="bookCard__text">{{article.description}}</p></div>
+                    <div><p class="bookCard__text bookCard__text_level">Рівень: {{getArticleLevel(article.level)}} , {{article.parts.length}} частин</p> </div>
+                </div>
+            </v-card-title>
+            <div class="bookCard__btn">
                 <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn class="primary" flat :to="{name:'article', params:{id:article.id}}">Читати</v-btn>
-                </v-card-actions>
-            </v-flex> -->
-                <v-card class="mr-4">
-                    <v-card-media :src="article.imageUrl" height="300px"></v-card-media>
-                    <v-card-title primary-title>
-                    <div>
-                        <h3 class="headline mb-0">{{article.title}}</h3>
-                        <div> <p>{{article.description}}</p> </div>
-                        <div><p>Рівень: {{getArticleLevel(article.level)}} , {{article.parts.length}} частей</p> </div>
-                    </div>
-                    </v-card-title>
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn class="primary" flat :to="{name:'article', params:{id:article.id}}">Читати</v-btn>
-                    </v-card-actions>
-                </v-card>
+                    <v-btn class="cyan darken-4" dark :to="{name:'article', params:{id:article.id}}">
+                        <v-icon class="pr-2">visibility</v-icon>
+                        Читати</v-btn>
+                </v-card-actions> 
+            </div>
+        </div>
+    </v-card>
 </template>
 
 <script>
@@ -48,5 +38,46 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .bookCard{
+        position: relative;
+        width:320px;
+        cursor: pointer;
+        box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.45);
+        &:hover .bookCard__titleWrap{
+            opacity: 1;
+        }
+    }
+    .bookCard__titleWrap{
+        position: absolute;
+        top:0;
+        bottom:0;
+        left: 0;
+        right:0;
+        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: rgba(0,0,0,.6);
+        padding: 10px;
+        transition: .5s;
+        opacity: 0;
+    }
+    .bookCard__text{
+        color:#fff;
+        &_level{
+            margin-top: 30px;
+        }
+    }
+    .bookCard__captionWrap{
+        position: absolute;
+        width:100%;
+        left:0;
+        top:20px;
+        text-align:center;
+    }
+    .bookCard__btn{
+        position:absolute;
+        bottom: 20px;
+    }
 </style>
